0e5e15d2098fd428a75ffe3113f2570925933c15
Michele Ceriotti <michele.ceriotti@gmail.com>
2022-07-09 00:24:46 +0200

    Force fit MIXED WITH ENERGIES!!!!

--- multibody-force.py
+++ multibody-force.py
@@ -1,4 +1,4 @@
-#command-line version of the HEA-multibody notebook
+#command-line version of the HEA-multibody notebook (with force learning)
 
 import copy
 import cProfile
@@ -18,12 +18,15 @@ from utils.soap import PowerSpectrum, CompositionFeatures
 
 torch.set_default_dtype(torch.float64)
 
-n_test = 1000
-n_train = 5003
+n_test = 10
+n_train = 80
+n_train_forces = 20
+prefix = "sigma0.25"
 
-frames = ase.io.read("data/data_shuffle.xyz", f":{n_test + n_train}")
+frames = ase.io.read("data/data_shuffle.xyz", f":{n_test + n_train + n_train_forces}")
 
 train_frames = frames[:n_train]
+train_forces_frames = frames[n_train:n_train+n_train_forces]
 test_frames = frames[-n_test:]
 
 train_energies = torch.tensor(
@@ -34,9 +37,14 @@ test_energies = torch.tensor(
     [frame.info["energy"] for frame in test_frames]
 ).reshape(-1, 1).to(dtype=torch.get_default_dtype())
 
-train_forces = [
+
+train_forces_e = torch.tensor(
+    [frame.info["energy"] for frame in train_forces_frames]
+).reshape(-1, 1).to(dtype=torch.get_default_dtype())
+
+train_forces_f = [
     torch.tensor(frame.arrays["forces"]).to(dtype=torch.get_default_dtype())
-    for frame in train_frames
+    for frame in train_forces_frames
 ]
 
 test_forces = [
@@ -44,7 +52,7 @@ test_forces = [
     for frame in test_frames
 ]
 
-print(f"using {n_train} training frames")
+print(f"using {n_train} training frames and {n_train_forces} force frames")
 
 
 all_species = set()
@@ -70,7 +78,7 @@ HYPERS_SMALL = {
     "cutoff": 4.0,
     "max_angular": 3,
     "max_radial": 5,
-    "atomic_gaussian_width": 0.4,
+    "atomic_gaussian_width": 0.3,
     "cutoff_function": {"ShiftedCosine": {"width": 0.5}},
     "radial_basis": {"SplinedGto": {"accuracy": 1e-6}},
     "gradients": False,
@@ -91,7 +99,7 @@ HYPERS_RADIAL = {
     "cutoff": 6.0,
     "max_angular": 0,
     "max_radial": 12,
-    "atomic_gaussian_width": 0.4,
+    "atomic_gaussian_width": 0.3,
     "cutoff_function": {"ShiftedCosine": {"width": 0.5}},
     "radial_basis": {"SplinedGto": {"accuracy": 1e-6}},
     "gradients": False,
@@ -105,33 +113,39 @@ device = "cpu"
 #if torch.cuda.is_available():
 #    device = "cuda"
 
-
 train_dataset = AtomisticDataset(train_frames, all_species, 
                                  {"radial_spectrum": HYPERS_RADIAL, "spherical_expansion":HYPERS_SMALL}, train_energies)
+train_forces_dataset = AtomisticDataset(train_forces_frames, all_species, 
+                                 {"radial_spectrum": HYPERS_RADIAL, "spherical_expansion":HYPERS_SMALL}, train_forces_e)
 test_dataset = AtomisticDataset(test_frames, all_species, 
-                                {"radial_spectrum": HYPERS_RADIAL, "spherical_expansion":HYPERS_SMALL}, test_energies)
-
+                                {"radial_spectrum": HYPERS_RADIAL, "spherical_expansion":HYPERS_SMALL}, test_energies)                                
 do_gradients = True
 if do_gradients is True:
     HYPERS_GRAD = copy.deepcopy(HYPERS_SMALL)
     HYPERS_GRAD["gradients"] = do_gradients
     HYPERS_RAD_GRAD = copy.deepcopy(HYPERS_RADIAL)
     HYPERS_RAD_GRAD["gradients"] = do_gradients
-    train_dataset_grad = AtomisticDataset(train_frames, all_species, 
+    train_forces_dataset_grad = AtomisticDataset(train_forces_frames, all_species, 
                                           {"radial_spectrum": HYPERS_RAD_GRAD, "spherical_expansion":HYPERS_GRAD}, 
-                                          train_energies, train_forces)
+                                          train_forces_e, train_forces_f)
     test_dataset_grad = AtomisticDataset(test_frames, all_species, 
                                          {"radial_spectrum": HYPERS_RAD_GRAD, "spherical_expansion":HYPERS_GRAD}, 
                                          test_energies, test_forces)
 else:
-    train_dataset_grad = train_dataset
+    train_forces_dataset_grad = train_forces_dataset
     test_dataset_grad = test_dataset
-    
     
 train_dataloader = create_dataloader(
     train_dataset,
-    batch_size=800,
-    shuffle=False,    # NB: there's a strange bug when setting this to true
+    batch_size=200,
+    shuffle=True,    
+    device=device,
+)
+
+train_forces_dataloader = create_dataloader(
+    train_forces_dataset,
+    batch_size=200,
+    shuffle=True,    
     device=device,
 )
 
@@ -142,8 +156,15 @@ train_dataloader_no_batch = create_dataloader(
     device=device,
 )
 
-train_dataloader_single_frame = create_dataloader(
-    train_dataset,
+train_forces_dataloader_no_batch = create_dataloader(
+    train_forces_dataset,
+    batch_size=len(train_dataset),
+    shuffle=False,
+    device=device,
+)
+
+train_forces_dataloader_single_frame = create_dataloader(
+    train_forces_dataset,
     batch_size=1,
     shuffle=False,
     device=device,
@@ -151,28 +172,29 @@ train_dataloader_single_frame = create_dataloader(
 
 test_dataloader = create_dataloader(
     test_dataset,
-    batch_size=50,
+    batch_size=200,
     shuffle=False,
     device=device,
 )
+
 if do_gradients is True:
-    train_dataloader_grad = create_dataloader(
-        train_dataset_grad,
+    train_forces_dataloader_grad = create_dataloader(
+        train_forces_dataset_grad,
         batch_size=50,
         shuffle=True,
         device=device,
     )
 
-    train_dataloader_grad_no_batch = create_dataloader(
-        train_dataset_grad,
-        batch_size=len(train_dataset_grad),
-        shuffle=False,
+    train_forces_dataloader_grad_single_frame = create_dataloader(
+        train_forces_dataset_grad,
+        batch_size=1,
+        shuffle=True,
         device=device,
     )
-
-    train_dataloader_grad_single_frame = create_dataloader(
-        train_dataset_grad,
-        batch_size=1,
+    
+    train_forces_dataloader_grad_no_batch = create_dataloader(
+        train_forces_dataset_grad,
+        batch_size=len(train_forces_dataset_grad),
         shuffle=False,
         device=device,
     )
@@ -184,10 +206,11 @@ if do_gradients is True:
         device=device,
     )
 else:
-    train_dataloader_grad_single_frame = train_dataloader_single_frame
-    train_dataloader_grad = train_dataloader
-    train_dataloader_grad_no_batch = train_dataloader_no_batch
+    train_forces_dataloader_grad = train_forces_dataloader
+    train_forces_dataloader_grad_single_frame = train_forces_dataloader_single_frame
+    train_forces_dataloader_grad_no_batch = train_forces_dataloader_no_batch
     test_dataloader_grad = test_dataloader
+
 
 def loss_mae(predicted, actual):
     return torch.sum(torch.abs(predicted.flatten() - actual.flatten()))
@@ -369,7 +392,7 @@ else:
 model.to(device=device, dtype=torch.get_default_dtype())
 
 if model.random_initial_weights:
-    dataloader_initialization = train_dataloader_grad_single_frame
+    dataloader_initialization = train_forces_dataloader_grad_single_frame
 else:
     dataloader_initialization = train_dataloader_no_batch
     
@@ -390,8 +413,9 @@ optimizer = torch.optim.LBFGS(model.parameters(), lr=lr)#, line_search_fn="stron
 
 all_losses = []
 all_tests=[]
+f_all_tests=[]
 
-filename = f"{model.__class__.__name__}-{N_PSEUDO_SPECIES}-mixed-{n_train}-train"
+filename = f"{prefix}-{model.__class__.__name__}-{N_PSEUDO_SPECIES}-mixed-{n_train}-train-{n_train_forces}-f"
 if model.optimizable_weights:
     filename += "-opt-weights"
 
@@ -399,7 +423,7 @@ if model.random_initial_weights:
     filename += "-random-weights"
 
 output = open(f"{filename}.dat", "w")
-output.write("# epoch  train_loss  test_mae\n")
+output.write("# epoch  train_loss  test_mae test_mae_f\n")
 n_epochs_total = 0
 
 torch.save(model.state_dict(), f"{filename}-init.torch")
@@ -448,6 +472,49 @@ for epoch in range(300):
     all_losses.append(loss)
 
     epoch_time = time.time() - epoch_start
+    def single_step():
+        global composition, radial_spectrum, spherical_expansions, energies
+        optimizer.zero_grad()
+        if device=="cuda":
+            print(f"mem. before:  {torch.cuda.memory_stats()['allocated_bytes.all.current']/1e6} MB allocated, {torch.cuda.memory_stats()['reserved_bytes.all.current']/1e6} MB reserved ")
+        loss = torch.zeros(size=(1,), device=device)
+        loss_force = torch.zeros(size=(1,), device=device)
+        if himem:
+            predicted, _ = model(composition, radial_spectrum, spherical_expansions, forward_forces=False)
+            loss += loss_mse(predicted, energies)
+            f_predicted_e, f_predicted_f = model(f_composition, f_radial_spectrum, f_spherical_expansions, forward_forces=do_gradients)
+            loss += loss_mse(f_predicted_e, f_energies)
+            if do_gradients:
+                loss_force += loss_mse(f_predicted_f, f_forces)/42
+        else:
+            for composition, radial_spectrum, spherical_expansions, energies, forces in train_dataloader:
+                try:
+                    predicted, _ = model(composition, radial_spectrum, spherical_expansions, forward_forces=False)
+                except:
+                    if device=="cuda":
+                        print(f"mem. during:  {torch.cuda.memory_stats()['allocated_bytes.all.current']/1e6} MB allocated, {torch.cuda.memory_stats()['reserved_bytes.all.current']/1e6} MB reserved ")
+                    raise
+                loss += loss_mse(predicted, energies)
+            raise ValueError("MUST IMPLEMENT FORCE CALCULATOR FOR THIS PATH!")
+        loss /= n_train
+        if model.composition_model is not None:
+            loss += TORCH_REGULARIZER_COMPOSITION * torch.linalg.norm(model.composition_model.weights)
+        if model.radial_spectrum_model is not None:
+            loss += TORCH_REGULARIZER_RADIAL_SPECTRUM * torch.linalg.norm(model.radial_spectrum_model.weights)
+        if model.power_spectrum_model is not None:
+            loss += TORCH_REGULARIZER_POWER_SPECTRUM * torch.linalg.norm(model.power_spectrum_model.weights)
+
+        print(f"Train loss: {(loss+loss_force).item()} E={loss.item()}, F={loss_force.item()}")
+        loss+=loss_force
+        loss.backward(retain_graph=False)
+        print("Loss gradient", np.linalg.norm(model.composition_model.weights.grad.numpy()))
+        return loss
+            
+    loss = optimizer.step(single_step)
+    loss = loss.item()
+    all_losses.append(loss)
+
+    epoch_time = time.time() - epoch_start
     if epoch % 1 == 0:
         print("norms", np.linalg.norm(0 if model.composition_model is None else model.composition_model.weights.detach().cpu().numpy()),
                   np.linalg.norm(0 if model.radial_spectrum_model is None else model.radial_spectrum_model.weights.detach().cpu().numpy()),
@@ -461,31 +528,41 @@ for epoch in range(300):
         with torch.no_grad():
             predicted = []
             reference = []
-            f_reference = []
             f_predicted = []
+            f_reference = []
             for tcomposition, tradial_spectrum, tspherical_expansions, tenergies, tforces in test_dataloader_grad:
                 reference.append(tenergies)
-                f_reference.append(tforces)
-                predicted_e, predicted_f = model(tcomposition, tradial_spectrum, tspherical_expansions, forward_forces=True)
-                predicted.append(predicted_e)
-                f_predicted.append(predicted_f)
-                
+                tpredicted_e, tpredicted_f = model(tcomposition, tradial_spectrum, tspherical_expansions, forward_forces=do_gradients)
+                predicted.append(tpredicted_e)                
+                if do_gradients:
+                    f_predicted.append(tpredicted_f)
+                    f_reference.append(tforces)
+
             reference = torch.vstack(reference)
             predicted = torch.vstack(predicted)
-            f_reference = torch.vstack(f_reference)
-            f_predicted = torch.vstack(f_predicted)
-
             test_mae = loss_mae(predicted, reference)/n_test
-            test_f_mae = loss_mae(f_predicted, f_reference)/n_test/42
-
-            output.write(f"{n_epochs_total} {loss} {test_mae}\n")
+            output.write(f"{n_epochs_total} {loss} {test_mae}")
+            if do_gradients:
+                f_reference = torch.vstack(f_reference)
+                f_predicted = torch.vstack(f_predicted)
+                f_test_mae = loss_mae(f_predicted, f_reference)/n_test/42
+                output.write(f"{f_test_mae}")                
+            output.write("\n")
             output.flush()
         all_tests.append(test_mae.item())
+        f_all_tests.append(f_test_mae.item())
+        print(f"epoch {n_epochs_total} took {epoch_time:.4}s, optimizer loss={loss:.4}, test mae={test_mae:.4}"+
+              (f" test mae force={f_test_mae:.4}"))
+        all_tests.append(test_mae.item())
         print(f"epoch {n_epochs_total} took {epoch_time:.4}s, optimizer loss={loss:.4}, test mae={test_mae:.4}, test force mae={test_f_mae:.4} ")
+        np.savetxt(f"{filename}-energy_test.dat",np.hstack([reference.cpu().numpy(), predicted.cpu().numpy()]))
+        np.savetxt(f"{filename}-force_test.dat",np.hstack([f_reference.cpu().numpy(), f_predicted.cpu().numpy()]))
     del loss
     n_epochs_total += 1
-    
+    torch.save(model.state_dict(), f"{filename}-restart.torch")
+
 torch.save(model.state_dict(), f"{filename}-final.torch")
+
 
 with torch.no_grad():
     tpredicted = []
@@ -498,6 +575,6 @@ with torch.no_grad():
     treference = torch.vstack(treference)
     tpredicted = torch.vstack(tpredicted)
     tmae = loss_mae(tpredicted, treference)/n_train
+
 print(f"TRAIN MAE: {tmae.item()/42} eV/at")    
 print(f"TEST MAE: {test_mae.item()/42} eV/at")
-
